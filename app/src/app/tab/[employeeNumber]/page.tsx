4475086c62b0df03d3a6817fcfcada9a8febdab5
Samuel Grenier <samuel.grenier@bell.ca>
2026-05-29 15:50:53 -0400

    fix keyboard

--- app/src/app/tab/[employeeNumber]/page.tsx
+++ app/src/app/tab/[employeeNumber]/page.tsx
@@ -89,6 +89,9 @@ export default function TabPage({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [employeeNumber]);
 
+  useEffect(() => {
+    scanInputRef.current?.focus();
+  }, []);
   // Handle product barcode scan
   const handleProductScan = useCallback(
     async (barcode: string) => {
@@ -354,7 +357,7 @@ export default function TabPage({
           ref={scanInputRef}
           value={scanValue}
           onBlur={() => {
-            if (!saveOpen && !resetOpen && !unknownOpen) {
+            if (!saveOpen && !resetOpen && !unknownOpen && !editProduct) {
               scanInputRef.current?.focus();
             }
           }}
@@ -392,7 +395,6 @@ export default function TabPage({
           w={0}
           overflow="hidden"
           inputMode="none"
-          autoFocus
         />
 
         {/* Fixed height container for scan feedback and products list to prevent UI shifting */}
