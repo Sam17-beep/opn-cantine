256b8b62537b33bfb682aa20d697fee01d22149c
Samuel Grenier <78397416+Sam17-beep@users.noreply.github.com>
2026-06-11 14:19:06 -0400

    fix (#24)
    Co-authored-by: Samuel Grenier <samuel.grenier@bell.ca>

--- app/src/app/tab/[employeeNumber]/page.tsx
+++ app/src/app/tab/[employeeNumber]/page.tsx
@@ -329,7 +329,7 @@ export default function TabPage({
               fontWeight="600"
               fontSize={{ base: 'xl', md: '2xl' }}
             >
-              {hasPending ? 'Sauvegarder' : 'Déconection'}
+              {hasPending ? 'Sauvegarder' : 'Déconnexion'}
             </Button>
             <Button
               flex={{ md: 1 }}
